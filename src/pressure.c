3904fa18f6d0903064615fc96febaf7a6a9f6e6b
Craig Wang <craigwang1@gmail.com>
2023-06-30 17:16:36 -0700

    Make pressure not stop everything else if pressure is not connected

--- src/pressure.c
+++ src/pressure.c
@@ -42,15 +42,16 @@ void pressure_thread(void *arg1, void *arg2, void *arg3){
     LOG_DBG("Started pressure thread");
     static const struct device *const dev = DEVICE_DT_GET_ANY(meas_ms5837);
     static struct sensor_value oversampling_rate = { 8192, 0 };
+    bool good = true;
 
     if (dev == NULL) {
         LOG_ERR("Could not find MS5837 device, aborting test.");
-        return;
+        good = false;
     }
     if (!device_is_ready(dev)) {
         LOG_ERR("MS5837 device %s is not ready, aborting test.",
                 dev->name);
-        return;
+        good = false;
     }
 
     if (sensor_attr_set(dev, SENSOR_CHAN_ALL, SENSOR_ATTR_OVERSAMPLING,
@@ -58,7 +59,7 @@ void pressure_thread(void *arg1, void *arg2, void *arg3){
         LOG_ERR("Could not set oversampling rate of %d "
                 "on MS5837 device, aborting test.",
                 oversampling_rate.val1);
-        return;
+        good = false;
     }
 
     struct sensor_value press;
@@ -68,27 +69,29 @@ void pressure_thread(void *arg1, void *arg2, void *arg3){
 
 
     while (1) {
-        ret = sensor_sample_fetch(dev);
-        if (ret != 0) {
-            LOG_DBG("Fetch error %d", ret);
-            continue;
-        }
-        ret = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
-        if (ret != 0) {
-            LOG_DBG("Sensor get error %d", ret);
-            continue;
-        }
+    	if (good)
+    	{
+	        ret = sensor_sample_fetch(dev);
+	        if (ret != 0) {
+	            LOG_DBG("Fetch error %d", ret);
+	            continue;
+	        }
+	        ret = sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
+	        if (ret != 0) {
+	            LOG_DBG("Sensor get error %d", ret);
+	            continue;
+	        }
 
-        pressure_sample = press.val1 + press.val2 * pow(10, -6);
-        pressure_data.depth = (pressure_sample - 5.123) * 1.997;
-        while (k_msgq_put(&pressure_data_msgq, &pressure_data, K_NO_WAIT) != 0) {
-            k_msgq_put(&pressure_data_msgq, &pressure_data, K_NO_WAIT);
-        }
-        LOG_DBG("Pressure reading: %f", pressure_sample);
+	        pressure_sample = press.val1 + press.val2 * pow(10, -6);
+	        pressure_data.depth = (pressure_sample - 5.123) * 1.997;
+	        while (k_msgq_put(&pressure_data_msgq, &pressure_data, K_NO_WAIT) != 0) {
+	            k_msgq_put(&pressure_data_msgq, &pressure_data, K_NO_WAIT);
+	        }
+	        LOG_DBG("Pressure reading: %f", pressure_sample);
+    	}
         k_sleep(K_MSEC(100));
     }
 }
-
 
 K_THREAD_DEFINE(pressure_thread_id, 2048,
                 pressure_thread, NULL, NULL, NULL,
